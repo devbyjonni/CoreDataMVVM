daeb65c05a66bad9a63c1144a47be304668599ca
Jonni <enterTheSource@me.com>
2023-02-25 18:53:45 +0100

    Initial Commit

---
+++ CoreDataMVVM/CoreDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMVVMApp.swift
+//  CoreDataMVVM
+//
+//  Created by Jonni Akesson on 2023-02-25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
